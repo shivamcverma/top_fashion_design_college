327f3a6be2cdfd3f50256b0e26268f68d333825f
Shivam Verma <sv726544@gmail.com>
2026-01-19 17:01:34 +0530

    logic

---
+++ listingcollege/api.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI, HTTPException
+import json, os
+
+app = FastAPI(title="fashion design Colleges API")
+
+DATA_FILE = "top_fashion_design_college_data.json"
+
+def load_data():
+    if os.path.exists(DATA_FILE):
+        with open(DATA_FILE, "r", encoding="utf-8") as f:
+            return json.load(f)
+    return []
+
+@app.get("/")
+async def root():
+    return {"message": "API is running! Go to /fasion_design_colleges to see all colleges."}
+
+@app.get("/fashion_design")
+async def get_all_colleges():
+    return {"/fashion_design": load_data()}
+
+@app.get("/fashion_design/{college_id}")
+async def get_college_by_id(college_id: int):
+    data = load_data()
+    idx = 1
+    for section in data:
+        for college in section["colleges"]:
+            if idx == college_id:
+                return college
+            idx += 1
+    raise HTTPException(status_code=404, detail="College not found")
